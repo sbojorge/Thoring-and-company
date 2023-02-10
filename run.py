bd6ae8cd49a7026182f1ca37f948365d4b6f370c
sbojorge <sara.bojorge@gmail.com>
2023-02-10 11:49:00 +0000

    Create a json file and retrieve its information using a for loop

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -12,8 +13,11 @@ def index():
 
 @app.route("/about")
 def about():
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
     return render_template("about.html", page_title="About",
-    list_of_numbers=[1, 2, 3])
+    company=data)
 
 
 @app.route("/contact.html")
